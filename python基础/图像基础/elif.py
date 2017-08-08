b005c41b0f8a3549d9751e2a57443d4f26bee9a9
510850111 <510850111@qq.com>
2017-08-08 10:42:28 +0800

    将elif的代码从2.7更新到3.5版本

--- python基础/图像基础/elif.py
+++ python基础/图像基础/elif.py
@@ -1,8 +1,8 @@
-x = int(raw_input("please input an integer:"))
-if x < 0
-	x = 0
-	print '已经将您输入的数字改成了0'
-elif x == 0
-	print '您输入的数字是零'
-else
-	print '您输入的数字大于零'
+x = int(input("please input an integer:"))
+if x < 0:
+    	x = 0
+    	print ('已经将您输入的数字改成了0')
+elif x == 0:
+	print('您输入的数字是零')
+else:
+	print('您输入的数字大于零')
